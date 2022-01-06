e8759673a5ae98e70be591664134b78131a993fd
Brocha Eckstein <91932664+Brochae@users.noreply.github.com>
2022-01-05 22:44:42 -0500

    Update RealCovidStats specs.sql

--- RealCovidStats specs.sql
+++ RealCovidStats specs.sql
@@ -71,6 +71,10 @@ I heard you have a table called Medalists.
     -Medalists whose first name starts with a-k got the 1/2 wave
     -Medalists whose first name starts with l-z got delta
     -Medalists whose last name start with g-q got omicron
+-For the exact date that the medalist contracted the disease:
+    -April 14, 2020 for the 1/2 wave
+    -August 20, 2021 for the Delta wave
+    -November 26, 2021 for the Omicron wave
 -For the Degree of Symptoms:
     -Medalists whose first name contains 1-4 characters - Mild
     -Medalists whose first name contains 5-6 characters - Moderate
